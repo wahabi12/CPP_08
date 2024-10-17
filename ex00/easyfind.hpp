21f5b56c5bae1e3c0bd2b6d2fa43eb1f1d6b42da
blatifat <blatifat@student.42heilbronn.de>
2024-10-17 04:54:54 +0200

    ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blatifat <blatifat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 22:12:11 by blatifat          #+#    #+#             */
+/*   Updated: 2024/10/17 04:54:30 by blatifat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <exception>
+#include <iterator>
+#include <vector>
+#include <list>
+
+class NotFoundException : public std::exception {
+public:
+	const char* what() const throw() {
+		return "Value not found in the container";
+	}
+};
+
+template <typename T>
+typename T::iterator easyfind(T& container, int value) {
+	typename T::iterator it = std::find(container.begin(), container.end(), value);
+	if (it == container.end()) {
+		throw NotFoundException();
+	}
+	return it;
+}
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blatifat <blatifat@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/06 22:12:31 by blatifat          #+#    #+#             */
+/*   Updated: 2024/10/17 04:54:35 by blatifat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+
+int main() {
+	// Test avec un std::vector
+	std::vector<int> vec;
+	for (int i = 0; i < 10; i++) {
+		vec.push_back(i);
+	}
+
+	std::cout << "Testing with std::vector:" << std::endl;
+	try {
+		std::vector<int>::iterator it = easyfind(vec, 5);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		std::vector<int>::iterator it = easyfind(vec, 42);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	// Test avec un std::list
+	std::list<int> lst;
+	for (int i = 0; i < 10; i++) {
+		lst.push_back(i * 2);
+	}
+
+	std::cout << "Testing with std::list:" << std::endl;
+	try {
+		std::list<int>::iterator it = easyfind(lst, 4);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		std::list<int>::iterator it = easyfind(lst, 3);
+		std::cout << "Value found: " << *it << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
